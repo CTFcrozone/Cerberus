803f770503c87a16fe42edefbcfca7180eff3a93
χ <voidbyte@null.net>
2026-03-21 21:13:01 +0100

    remove timestamp shardkey

--- crates/libs/lib-rules/src/engine/correlator/sharded.rs
+++ crates/libs/lib-rules/src/engine/correlator/sharded.rs
@@ -7,7 +7,7 @@ use crate::{
 	rule::Sequence,
 };
 
-type ShardKey = (u32 /* pid */, u64 /* ts */);
+type ShardKey = u32; // pid
 
 pub struct ShardedCorrelator {
 	shards: DashMap<ShardKey, Correlator>,
@@ -18,25 +18,24 @@ impl ShardedCorrelator {
 		Self { shards: DashMap::new() }
 	}
 
-	fn get_correlator(&self, pid: u32, ts: u64) -> dashmap::mapref::entry::Entry<'_, ShardKey, Correlator> {
-		self.shards.entry((pid, ts))
+	fn get_correlator(&self, pid: u32) -> dashmap::mapref::entry::Entry<'_, ShardKey, Correlator> {
+		self.shards.entry(pid)
 	}
 
-	pub fn on_root_match(&self, pid: u32, ts: u64, root_rule_id: &str, seq: &Sequence, now: Instant) {
-		let mut correlator = self.get_correlator(pid, ts).or_insert_with(Correlator::new);
+	pub fn on_root_match(&self, pid: u32, root_rule_id: &str, seq: &Sequence, now: Instant) {
+		let mut correlator = self.get_correlator(pid).or_insert_with(Correlator::new);
 		correlator.on_root_match(root_rule_id, seq, now);
 	}
 
 	pub fn on_rule_match(
 		&self,
 		pid: u32,
-		ts: u64,
 		matched_rule_id: &str,
 		seq: &Sequence,
 		root_rule_id: &str,
 		now: Instant,
 	) -> Option<CorrelatedMatch> {
-		let mut correlator = self.shards.get_mut(&(pid, ts))?;
+		let mut correlator = self.shards.get_mut(&pid)?;
 		correlator.on_rule_match(matched_rule_id, seq, root_rule_id, now)
 	}
 }
